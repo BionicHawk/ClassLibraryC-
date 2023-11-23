2b11a13cca1f757e472f0a243cb17b87d94ef622
BionicHawk <angel.manzo.rosas@gmail.com>
2023-11-23 02:02:42 -0600

    Agregar archivos de proyecto.

---
+++ ClassLibraryC#/COMPlusServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.EnterpriseServices;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ApplicationName("ComPlusExample")]
+[assembly: AssemblyKeyFile("bin/ComPlusExample.snk")]
+namespace ClassLibraryC_
+{
+    [ComVisible(true)]
+    [Transaction(TransactionOption.Required)]
+    public class COMPlusServices : ServicedComponent
+    {
+
+        public COMPlusServices() : base() { 
+    
+        }
+
+        [AutoComplete()]
+        public string DoTransaction()
+        {
+            return "COM+ funciona correctamente";
+        }
+
+    }
+}
